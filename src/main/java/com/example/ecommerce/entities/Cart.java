8cfc87ebce9a0694dbc50acef913009e6c8925e8
Vitor Paulino <vitorpaulinog@gmail.com>
2024-10-28 18:43:18 -0300

    Add: data mapping to Cart entity

--- src/main/java/com/example/ecommerce/entities/Cart.java
+++ src/main/java/com/example/ecommerce/entities/Cart.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "tb_Cart")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "customerId", nullable = false, foreignKey = @ForeignKey(name = "fk_cart_customer"))
     private Customer customer;
 }
