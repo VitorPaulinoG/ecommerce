2a189d3e7f6a2061f2ccda4f48aa1dd7c115f41f
Vitor Paulino <vitorpaulinog@gmail.com>
2024-10-28 18:43:56 -0300

    Add: data mapping to Address entity

--- src/main/java/com/example/ecommerce/entities/Address.java
+++ src/main/java/com/example/ecommerce/entities/Address.java
@@ -1,18 +1,41 @@
 package com.example.ecommerce.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "tb_Address")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "cep", nullable = true, columnDefinition = "char(8)")
+    private String cep;
+
+    @Column(name = "street", nullable = false, columnDefinition = "varchar(500)")
     private String street;
-    private String number;
+
+    @Column(name = "number", nullable = false, columnDefinition = "int")
+    private int number;
+
+    @Column(name = "complement", nullable = false, columnDefinition = "varchar(50)")
+    private String complement;
+
+    @Column(name = "city", nullable = false, columnDefinition = "varchar(255)")
     private String city;
+
+    @Column(name = "state", nullable = false, columnDefinition = "varchar(500)")
     private String state;
+
+    @Column(name = "country", nullable = false, columnDefinition = "varchar(500)")
     private String country;
 }
