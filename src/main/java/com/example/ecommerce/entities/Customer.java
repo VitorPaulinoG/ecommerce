19567fd44ffc66c14dccc58bca7172aba376a4e6
Vitor Paulino <vitorpaulinog@gmail.com>
2024-10-28 18:40:43 -0300

    Add: data mapping to Customer entity

--- src/main/java/com/example/ecommerce/entities/Customer.java
+++ src/main/java/com/example/ecommerce/entities/Customer.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,13 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "tb_Customer")
 public class Customer extends User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "addressId", nullable = false, foreignKey = @ForeignKey(name = "fk_customer_address"))
     private Address address;
 }
